84416cce4d9d2e69ed870a236817e55f22aef929
Diksha Kumari Singh <130738814+Diksha201@users.noreply.github.com>
2025-06-05 19:16:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { store } from "./redux/Store";
@@ -11,12 +11,12 @@ import { Toaster } from "react-hot-toast";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <BrowserRouter basename="/PawKart">
+    <HashRouter>
         <Provider store={store}>
           <App />   
           <Toaster/>  
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
 
 
 
