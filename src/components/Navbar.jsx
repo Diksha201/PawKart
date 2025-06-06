bcd35d72933b66e910aed9a3641be6be84863c28
Diksha Kumari Singh <130738814+Diksha201@users.noreply.github.com>
2025-06-06 15:12:31 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
             placeholder="Search products..."
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            className="w-full px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-yellow-300"
+            className="w-full px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-yellow-400"
           />
           <img
             src="https://cdn-icons-png.flaticon.com/256/5680/5680244.png"
